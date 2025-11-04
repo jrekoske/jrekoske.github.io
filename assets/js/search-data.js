bbc7669420a94dcaa235819379092874a10b059b
jrekoske <58538930+jrekoske@users.noreply.github.com>
2025-11-04 04:30:34 +0000

    Deploying to gh-pages from @ jrekoske/jrekoske.github.io@6e695256a93f5d682bbc658c449054c54502ddc8 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -25,7 +25,7 @@ ninja.data = [{
           },
         },{id: "nav-cv",
           title: "CV",
-          description: "Please download this PDF to view my CV.",
+          description: "View my CV below or download it as a PDF.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
@@ -34,55 +34,6 @@ ninja.data = [{
           title: 'Our paper, Reduced-order modelling for complex three-dimensional seismic wave propagation, was just published...',
           description: "",
           section: "News",},{
-        id: 'social-email',
-        title: 'email',
-        section: 'Socials',
-        handler: () => {
-          window.open("mailto:%6A%72%65%6B%6F%73%6B%65@%75%63%73%64.%65%64%75", "_blank");
-        },
-      },{
-        id: 'social-github',
-        title: 'GitHub',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://github.com/jrekoske", "_blank");
-        },
-      },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/john-rekoske-042080165/", "_blank");
-        },
-      },{
-        id: 'social-orcid',
-        title: 'ORCID',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://orcid.org/0000-0003-0539-2069", "_blank");
-        },
-      },{
-        id: 'social-researchgate',
-        title: 'ResearchGate',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.researchgate.net/profile/John-Rekoske/", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=o3z1Cf0AAAAJ&hl", "_blank");
-        },
-      },{
-        id: 'social-x',
-        title: 'X',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://twitter.com/JohnRekoske", "_blank");
-        },
-      },{
       id: 'light-theme',
       title: 'Change theme to light',
       description: 'Change the theme of the site to Light',
